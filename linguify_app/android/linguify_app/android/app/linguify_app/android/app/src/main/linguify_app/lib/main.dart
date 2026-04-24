5b0d0ced12d864a3d62661020dda3e335b3e681c
mo2845040-oss <mo2845040@gmail.com>
2026-04-25 02:49:56 +0300

    Create main.dart
    linguify_app/lib/main.dart

---
+++ linguify_app/android/linguify_app/android/app/linguify_app/android/app/src/main/linguify_app/lib/main.dart
@@ -0,0 +1,47 @@
+import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'package:provider/provider.dart';
+import 'package:google_fonts/google_fonts.dart';
+import 'firebase_options.dart';
+import 'config/theme.dart';
+import 'config/constants.dart';
+import 'providers/auth_provider.dart';
+import 'providers/lesson_provider.dart';
+import 'providers/chat_provider.dart';
+import 'providers/leaderboard_provider.dart';
+import 'services/fcm_service.dart';
+import 'screens/splash_screen.dart';
+
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(options: DefaultFirebaseOptions.currentPlatform);
+  await SystemChrome.setPreferredOrientations([DeviceOrientation.portraitUp]);
+  SystemChrome.setSystemUIOverlayStyle(const SystemUiOverlayStyle(
+    statusBarColor: Colors.transparent,
+    statusBarIconBrightness: Brightness.dark,
+  ));
+  runApp(const LinguifyApp());
+}
+
+class LinguifyApp extends StatelessWidget {
+  const LinguifyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MultiProvider(
+      providers: [
+        ChangeNotifierProvider(create: (_) => AuthProvider()),
+        ChangeNotifierProvider(create: (_) => LessonProvider()),
+        ChangeNotifierProvider(create: (_) => ChatProvider()),
+        ChangeNotifierProvider(create: (_) => LeaderboardProvider()),
+      ],
+      child: MaterialApp(
+        title: AppConstants.appName,
+        debugShowCheckedModeBanner: false,
+        theme: AppTheme.lightTheme,
+        home: const SplashScreen(),
+      ),
+    );
+  }
+}
